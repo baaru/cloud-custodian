7f6ba94cb2d82efdac97c994071b49b0641eddc5
baaru <53831024+baaru@users.noreply.github.com>
2020-04-24 19:42:09 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup_kwargs = {
     'package_data': package_data,
     'install_requires': install_requires,
     'entry_points': entry_points,
-    'python_requires': '>=3.6,<4.0',
+    'python_requires': '>=2.7.17,<3.8',
 }
 
 
